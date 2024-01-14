9d01daaf7f7dd9cb42210a10bc54d674e38862bb
Aditi <92170627+Aditi020@users.noreply.github.com>
2024-01-14 17:54:02 +0530

    Done with palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,14 @@
 */
 
 function isPalindrome(str) {
+  str=str.toLowerCase()
+  if (str==str.split("").reverse().join(""))
   return true;
+  else
+    return false;
 }
 
 module.exports = isPalindrome;
+
+// let str="deified"
+// console.log(isPalindrome(str));
