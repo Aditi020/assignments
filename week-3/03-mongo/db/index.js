f7e07b0abaf1dbf9fe091f67a1ca3571d7c62fe5
Aditi020 <aditi.kumar.2.2.24@gmail.com>
2024-05-12 01:43:56 +0530

    Mongo Assignment

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,20 +1,46 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
-
+mongoose.connect(
+    "mongodb+srv://aditikumar2224:AK0MongoDB@cluster0.zie5hxe.mongodb.net/New_user_app",
+).then(() => {
+    console.log("Connected to MongoDB");
+})
+    .catch((e) => {
+        console.log(e);
+    });
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
-});
+
+        // name: String,
+        username: String,
+        pasword: String,
+    });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
-});
+        // name: String,
+        username: String,
+        pasword: String,
+        purchasedCourse: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Course"
+            }
+        ],
+    })
+
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
-});
+        title: String,
+        description:String,
+        price:Number,
+        imageLink:String
+
+    })
+
 
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -2,6 +2,25 @@
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
-}
+    async function userExists(username, password) {
+        const { Admin } = require("../db");
+        // should check in the database
+        
+        const username = req.headers.username;
+        const password = req.headers.password;
+        
+        const AdminExist = await Admin.findOne({
+            username: username,
+            password: password,
+        });
+        if (AdminExist) {
+            next();
+        } else {
+            res.status(403).send({
+                msg: "User Not Exist",
+            });
+        }
+    }
+} 
 
 module.exports = adminMiddleware;
